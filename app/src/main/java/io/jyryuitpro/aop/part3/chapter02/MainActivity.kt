7174db1a06d92cf662afe69b81b396253a6455b5
jyr <jyr@wavers.co.kr>
2022-06-02 18:14:30 +0900

    01. 기본 UI 구성하기

--- app/src/main/java/io/jyryuitpro/aop/part3/chapter02/MainActivity.kt
+++ app/src/main/java/io/jyryuitpro/aop/part3/chapter02/MainActivity.kt
@@ -2,10 +2,21 @@ package io.jyryuitpro.aop.part3.chapter02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+    private val viewPager: ViewPager2 by lazy {
+        findViewById(R.id.viewPager)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        viewPager.adapter = QuotesPagerAdapter(emptyList())
     }
 }
---
+++ app/src/main/java/io/jyryuitpro/aop/part3/chapter02/Quote.kt
@@ -0,0 +1,6 @@
+package io.jyryuitpro.aop.part3.chapter02
+
+data class Quote(
+    val quote: String,
+    val name: String
+)

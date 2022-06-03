c71aaf72e270ea31dddd8afd5835e4986af26895
jyr <jyr@wavers.co.kr>
2022-06-03 09:54:45 +0900

    05. 완성도 높이기

--- app/src/main/java/io/jyryuitpro/aop/part3/chapter02/MainActivity.kt
+++ app/src/main/java/io/jyryuitpro/aop/part3/chapter02/MainActivity.kt
@@ -2,23 +2,47 @@ package io.jyryuitpro.aop.part3.chapter02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import androidx.viewpager2.widget.ViewPager2
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import org.json.JSONArray
 import org.json.JSONObject
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
     private val viewPager: ViewPager2 by lazy {
         findViewById(R.id.viewPager)
     }
 
+    private val progressBar: ProgressBar by lazy {
+        findViewById(R.id.progressBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initViews()
         initData()
+    }
+
+    private fun initViews() {
+        viewPager.setPageTransformer { page, position ->
+            when {
+                position.absoluteValue >= 1F -> {
+                    page.alpha = 0F
+                }
+                position.absoluteValue == 0F -> {
+                    page.alpha = 1F
+                }
+                else -> {
+                    page.alpha  = 1F - (2 * position.absoluteValue)
+                }
+            }
+        }
     }
 
     private fun initData() {
@@ -29,6 +53,8 @@ class MainActivity : AppCompatActivity() {
                 }
         )
         remoteConfig.fetchAndActivate().addOnCompleteListener {
+            progressBar.visibility = View.GONE
+
             if (it.isSuccessful) {
                 val quotes = parseQuoutesJson(remoteConfig.getString("quotes"))
                 val isNameRevealed = remoteConfig.getBoolean("is_name_revealed")
@@ -58,10 +84,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun displayQuotesPager(quotes: List<Quote>, isNameRevealed: Boolean) {
-        viewPager.adapter = QuotesPagerAdapter(
+        val adapter = QuotesPagerAdapter(
             quotes,
             isNameRevealed
         )
+        viewPager.adapter = adapter
+        viewPager.setCurrentItem(adapter.itemCount / 2, false)
     }
 }
 
